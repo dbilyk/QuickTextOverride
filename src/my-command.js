368a69fb37c510ccbb09ca67101dc5abe86e2d23
Dmitri Bilyk <dmitri.bilyk@gmail.com>
2019-05-17 16:23:51 -0700

    swapped default Sketch UI dialog for custom layout.

--- src/my-command.js
+++ src/my-command.js
@@ -5,7 +5,7 @@ export default function(context) {
 
   //set selection
   if(context.selection.length > 0){
-    selection = selection = sketch.fromNative(context.selection[0])
+    selection = sketch.fromNative(context.selection[0])
 
     if(selection.type == "SymbolInstance" 
       && selection.overrides.length > 0
@@ -25,6 +25,40 @@ export default function(context) {
   
 }
 
+function Alert(){
+  
+  let viewWidth = 300, viewHeight = 60;
+  
+  //let alert = COSAlertWindow.new();
+  let alert = NSAlert.alloc().init()
+  
+  //alert.setIcon(NSImage.alloc().initByReferencingFile(plugin.urlForResourceNamed("rectangle@2x.png").path()));
+  
+  
+  // Creating dialog buttons
+  let updateButton = alert.addButtonWithTitle("Ok");
+  let cancelButton = alert.addButtonWithTitle("Cancel");
+  
+  
+  var view = NSView.alloc().initWithFrame(NSMakeRect(0, 0, viewWidth, viewHeight));
+  alert.setAccessoryView(view);
+  
+  //text field
+  let textField = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 300, 60));
+
+  //focuses the text field automatcially
+  alert.window().setInitialFirstResponder(textField)
+
+  view.addSubview(textField)
+  this.getCurrentValue = ()=> textField.stringValue()
+  this.show = (title,initialValue='')=>{
+    alert.setMessageText(title)
+    textField.setStringValue(initialValue)
+    
+    return alert.runModal()
+  }
+}
+
 
 function showUI(selection, textOverride){
   let UI = sketch.UI
@@ -33,8 +67,14 @@ function showUI(selection, textOverride){
   let currentOverrideValue = textOverride.value
   let isOverridden = (overrideName !== currentOverrideValue)?true:false
 
-  let userInput = UI.getInputFromUser(overrideName, {defaultValue:(isOverridden)?currentOverrideValue:""}, function(err, val){
-    if(err) return
-    selection.overrides[0].value = val
-  })
+  let dialog = new Alert()
+  let response = dialog.show(overrideName,currentOverrideValue)
+
+  //handle dialog
+  if(response == 1000){
+    selection.overrides[0].value = dialog.getCurrentValue()
+  }
+  else{
+    return
+  }
 }
