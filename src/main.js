a948335294a39fcf75d11f5eb75d4b5532f04bea
Dmitri Bilyk <dmitri.bilyk@gmail.com>
2019-06-06 00:13:04 -0700

    added some new features.  Todo: save initial value to persistent storage

--- src/main.js
+++ src/main.js
@@ -1,73 +1,96 @@
-import sketch from 'sketch'
-import WebView from "sketch-module-web-view"
+let sketch = require("sketch")
+let WebView = require("sketch-module-web-view")
 var Shape =  require("sketch/dom").Shape
 var Settings = require('sketch/settings')
 //var Rectangle = require('sketch/dom').Rectangle
 
-export default function(context) {
-  let qto = new QuickTextOverride(context)
+let pluginInstance
 
-  if(qto.isValidSelection()){
-    qto.assembleTextOverrides()
+//need to convert launching and closing into separate processes
+export default function() {
+  let existingWebView = WebView.fromId("000")
+  if(existingWebView){
+    existingWebView._panel.close()
+  }
+  else{
+    pluginInstance = new QuickTextOverride()
+    pluginInstance.assembleTextOverrides()
+  
+    if(pluginInstance.getOverrideList().length > 0){
+      pluginInstance.openWebView()
+    }
     
-    if(qto.getOverrideList().length > 0){
-      qto.openWebView()
+    
+  }
+}
+
+export function onSelectionChanged(){
+  let existingWebView = WebView.fromId("000")
+  if(!existingWebView) {return}
+  else{
+    existingWebView._panel.close()
+    pluginInstance = new QuickTextOverride()
+    pluginInstance.assembleTextOverrides()
+  
+    if(pluginInstance.getOverrideList().length > 0){
+      pluginInstance.openWebView()
     }
   }
 }
 
 
-function QuickTextOverride(context){
-  let ctx = context,
-      currentOverrideIndex,
-      selection = (context.selection[0]) ? sketch.fromNative(context.selection[0]) : null,
-      textOverrides = [],
-      overrideOutline = new Shape({
-        name:"currentOverride",
-        locked: true,
-        style:{
-          borders:[{color:'#4466FF44',thickness:2}]
-        }
-      }),
-      pluginUI = new PluginUI()
-      
+function QuickTextOverride(){
 
-  //is this a symbol with overrides?
-  this.isValidSelection = ()=>{
-    if(selection){
-      if(selection.type == "SymbolInstance" && selection.overrides.length > 0){
-        return true
-      }
-      else{
-        return false
-      } 
-    }
+  let getSelectedPage = ()=>{
+    return sketch.getSelectedDocument().selectedPage
+  }
+  
+  let getSelectedLayers = ()=>{
+    return sketch.getSelectedDocument().selectedLayers.layers
   }
 
-  this.assembleTextOverrides = (overrides = selection.overrides) => {
-    if(overrides.length == 0){
-      return
-    }
+  let currentOverrideIndex,
+      textOverrides = [],
+      // overrideOutline = new Shape({
+      //   name:"currentOverride",
+      //   locked: true,
+      //   style:{
+      //     borders:[{color:'#4466FF44',thickness:2}]
+      //   }
+      // }),
+      pluginUI = new PluginUI()
 
-    for (let i = 0; i <= overrides.length-1; i++){
-      let currentOverride = overrides[i]
+  this.assembleTextOverrides = () => {
+    let selection = getSelectedLayers()
 
-      if(currentOverride.property == "stringValue"){
-        textOverrides.push({
-          label:currentOverride.affectedLayer.name, 
-          override:currentOverride,
-          currentValue: currentOverride.value,
-          defaultValue: currentOverride.affectedLayer.text
+    for(let i = 0; i < selection.length; i++){
+      if(selection[i].type != "SymbolInstance") break
+      
+      if(selection[i].overrides.length == 0) break
+      
+      for (let j = 0; j < selection[i].overrides.length; j++){
+        let currentOverride = selection[i].overrides[j]
 
-        })
+        if(currentOverride.property == "stringValue"){
+          textOverrides.push({
+            label:currentOverride.affectedLayer.name, 
+            override:currentOverride,
+            currentValue: currentOverride.value,
+            defaultValue: currentOverride.affectedLayer.text
+
+          })
+        }
       }
     }
+
     return textOverrides
   }
 
   this.getOverrideList = () =>{
     return [...textOverrides]
   }
+
+  
 
   this.openWebView = () => {
     assignDelegatesToUI()
@@ -86,7 +109,9 @@ function QuickTextOverride(context){
       pluginUI.setPickerValues(pickerValues)
       pluginUI.setPlaceholders(placeholders)
       pluginUI.setSelectedOverride(currentOverrideIndex)
+      visualizeActiveOverride()
       pluginUI.setTextFieldContent(textOverrides[currentOverrideIndex].currentValue)
+
   }
   
   // this.addOutlineToDocument = ()=>{
@@ -119,6 +144,7 @@ function QuickTextOverride(context){
     currentOverrideIndex = (currentOverrideIndex + 1) % textOverrides.length
     pluginUI.setTextFieldContent(textOverrides[currentOverrideIndex].currentValue)
     pluginUI.setSelectedOverride(currentOverrideIndex)
+
   }
   this.goToPreviousOverride = ()=>{
     currentOverrideIndex = (currentOverrideIndex - 1 + textOverrides.length) % textOverrides.length
@@ -136,57 +162,62 @@ function QuickTextOverride(context){
 
   let assignDelegatesToUI = ()=>{
     //listen for key events and respond to them.
-    pluginUI.tabForwardDelegate  = onTabForward
-    pluginUI.tabBackwardDelegate = onTabBackward
-    pluginUI.enterDelegate       = onEnter
-    pluginUI.escapeDelegate      = onEscape
+    pluginUI.tabForwardDelegate    = onTabForward
+    pluginUI.tabBackwardDelegate   = onTabBackward
+    pluginUI.enterDelegate         = onEnter
+    pluginUI.escapeDelegate        = onEscape
     pluginUI.clickOverrideDelegate = onClickOverride
   }
 
   let onTabForward = (contentBeforeChange)=>{
     this.saveCurrentOverride(contentBeforeChange)
     this.goToNextOverride()
+    visualizeActiveOverride()
     
   }
-
+  
+  
   let onTabBackward = (contentBeforeChange)=>{
     this.saveCurrentOverride(contentBeforeChange)
     this.goToPreviousOverride()
-
-  }
+    visualizeActiveOverride()
     
+  }
+  
   
   let onEnter = (contentBeforeClose)=>{
     this.saveCurrentOverride(contentBeforeClose)
     pluginUI.close()
     
   }
-
+  
   let onEscape = ()=>{
-    
+    textOverrides[currentOverrideIndex].override.value = textOverrides[currentOverrideIndex].currentValue 
   }
-
+  
   let onClickOverride = (index,currentContent)=>{
     this.saveCurrentOverride(currentContent)
     this.goToOverride(index)
+    visualizeActiveOverride()
   }
-
+  
+  let visualizeActiveOverride = ()=>{
+    textOverrides[currentOverrideIndex].override.value = '▂▂▂▂▂▂▂▂'
+  }
 }
 
 
 function PluginUI(){
   console.log("_______________________________________________________________")
   let _ui
-  let windowWidth = 800,
+  let windowWidth  = 800,
       windowHeight = 200,
       screenHeight = NSHeight(NSScreen.mainScreen().frame()),
-      screenWidth = NSWidth(NSScreen.mainScreen().frame()),
-      xPosition = (screenWidth/2) - (windowWidth/2),
-      yPosition = (screenHeight) - (windowHeight/2)
-
-
-
-
+      screenWidth  = NSWidth(NSScreen.mainScreen().frame()),
+      xPosition    = (screenWidth/2) - (windowWidth/2),
+      yPosition    = (screenHeight) - (windowHeight/2),
+      parent = sketch.getSelectedDocument(),
+      windowID     = "000"
 
   let emit = (type,body, callback)=>{
 
@@ -226,8 +257,6 @@ function PluginUI(){
     settingsKey:"userSettings",
     lastRect: undefined
   }
-
-  let settings 
 
 
   //these get assigned by QTO object which is responsible for handling these actions and notifying the UI.
@@ -284,39 +313,47 @@ function PluginUI(){
   
 
   this.open = (callback)=>{
-    if(_ui !== undefined) return
     _settings = _getSettings() || _settings
+    let _existingWindow = WebView.fromId("000")
 
     //if we have a previous state for where the user had the window, we want to open it in the same place as last time, with the same size.
     if(_settings.lastRect){
       windowHeight = _settings.lastRect.height
-      windowWidth = _settings.lastRect.width
-      xPosition = _settings.lastRect.x
-      yPosition = _settings.lastRect.y
+      windowWidth  = _settings.lastRect.width
+      xPosition    = _settings.lastRect.x
+      yPosition    = _settings.lastRect.y
     }  
     
-    _ui = new WebView({
-      identifier:"000",
-      width:windowWidth,
-      height:windowHeight,
-      // minWidth:290,
-      // minHeight:330,
-      useContentSize: true,
-      x: xPosition,
-      y: yPosition,
-      alwaysOnTop:true,
-      title:"Quick Text Override",
-      backgroundColor:"#ffffffff"
-    })
-    _ui.loadURL(require("./index.html"))
-    _ui.setAlwaysOnTop(true,"modal")
-    _startListeners()
-    _startListeningForWindowChanges()
 
-    emit("SYNC_ACTIONS", _actions).then(
-      ()    => {callback()},
-      (err) => {error(err)}
-    )
+    if(!_existingWindow){
+      _ui = new WebView({
+        identifier     : windowID,
+        width          : windowWidth,
+        height         : windowHeight,
+        useContentSize : true,
+        x              : xPosition,
+        y              : yPosition,
+        alwaysOnTop    : true,
+        title          : "Quick Text Override",
+        backgroundColor: "#ffffffff",
+        parent: parent
+      })
+      _ui.loadURL(require("./index.html"))
+      _ui.setAlwaysOnTop(true,"floating")
+      _startListeners()
+      _startListeningForWindowChanges()
+  
+      emit("SYNC_ACTIONS", _actions).then(
+        ()    => {callback()},
+        (err) => {error(err)}
+      )
+    }
+    else{
+      _ui = _existingWindow
+      callback()
+    }
+
+
   }
 
   let _startListeningForWindowChanges = ()=>{
@@ -337,15 +374,15 @@ function PluginUI(){
   }
 
   let _saveWindowPosition = ()=>{
-    let bounds = _ui.getBounds()
-    _settings.lastRect = bounds
+    let bounds             = _ui.getBounds()
+        _settings.lastRect = bounds
     Settings.setSettingForKey(_settings.settingsKey,_settings)
 
   }
 
   let _saveWindowDimensions = ()=>{
-    let bounds = _ui.getBounds()
-    _settings.lastRect = bounds
+    let bounds             = _ui.getBounds()
+        _settings.lastRect = bounds
     Settings.setSettingForKey(_settings.settingsKey,_settings)
 
   }
@@ -361,20 +398,24 @@ function PluginUI(){
   }
 
   this.setPickerValues = (pickerValues)=>{
-    emit(_actions.setPickerValues, pickerValues).then(()=>{},(err)=>{console.log(err)})
+    emit(_actions.setPickerValues, pickerValues)
+    .then(()=>{},(err)=>{console.log(err)})
   }
 
   this.setPlaceholders = (placeholders)=>{
-    emit(_actions.setPlaceholders, placeholders).then(()=>{},(err)=>{console.log(err)})
+    emit(_actions.setPlaceholders, placeholders)
+    .then(()=>{},(err)=>{console.log(err)})
   }
 
   this.setSelectedOverride = (index) =>{
-    emit(_actions.setSelectedOverride,index).then(()=>{},(err)=>{console.log(err)})
+    emit(_actions.setSelectedOverride,index)
+    .then(()=>{},(err)=>{console.log(err)})
   }
 
 
   this.setTextFieldContent = (newContent)=>{
-      emit(_actions.updateTextFieldContent, newContent).then(()=>{},(err)=>{console.log(err)})
+      emit(_actions.updateTextFieldContent, newContent)
+      .then(()=>{},(err)=>{console.log(err)})
   }
 }
 
